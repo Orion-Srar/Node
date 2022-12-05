8131e94daf32711252b148992b975c7c0c536090
boyko <boykoandriy93@gmail.com>
2022-12-05 12:18:50 +0200

    hw3

--- router/user.router.js
+++ router/user.router.js
@@ -4,13 +4,10 @@ const controller = require('../controller/user.controller');
 const middleware = require('../middleware/user.middleware');
 
 router.get('/', controller.getAllUsers);
-
 router.post('/', middleware.isBodyValidCreate, controller.createUser);
 
 router.get('/:userId', middleware.isIdValid, middleware.checkIsUserExist, controller.getUserById);
-
 router.put('/:userId', middleware.isIdValid, middleware.isBodyValidUpdate, middleware.checkIsUserExist, controller.updateUserById);
-
 router.delete('/:userId', middleware.isIdValid, middleware.checkIsUserExist, controller.deleteUserById);
 
 module.exports = router;
